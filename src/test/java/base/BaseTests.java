3eb6af2e70d3605e122fab5992f1c4a7e045cb6f
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-22 13:15:43 -0600

    WebElement

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,7 +1,11 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
@@ -10,9 +14,16 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
+        WebElement ShiftLink = driver.findElement(By.linkText("Shifting Content"));
+        ShiftLink.click();
+        WebElement ExampleLink = driver.findElement(By.linkText("Example 1: Menu Element"));
+        ExampleLink.click();
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        System.out.println(links.size());
         System.out.println(driver.getTitle());
         driver.quit();
+
+
     }
 
     public static void main(String args[]){
