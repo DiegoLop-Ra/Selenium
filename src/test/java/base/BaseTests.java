c729333f68289dfc8c21d8244fa5276d3db77198
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-23 15:41:05 -0600

    Alertas

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.List;
 
@@ -15,12 +16,15 @@ public class BaseTests {
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        goHome();
         homePage = new HomePage(driver);
-
-
-
     }
+
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
     @AfterClass
     public void teardown(){
         driver.quit();
