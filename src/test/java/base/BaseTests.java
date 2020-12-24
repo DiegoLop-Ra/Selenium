3308ae0223b6120865bc9469e404724c4855f9a7
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-24 14:07:26 -0600

    Reporte

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
@@ -17,12 +19,14 @@ import java.io.IOException;
 import java.util.List;
 
 public class BaseTests {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
+
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
         goHome();
         homePage = new HomePage(driver);
     }
